8dc1cbff275acc17a0c5d4b6f45a966cb7316664
Bea Maia <beamaia.work@gmail.com>
2022-06-02 21:44:48 -0300

    :recycle: gets random animal
    Co-Authored-By: Sophie Dilhon <51868657+ahalic@users.noreply.github.com>

--- database/database_seed.py
+++ database/database_seed.py
@@ -1,10 +1,11 @@
 import psycopg2
 import names
+import random
 
 from animals import Animal
 from env import DB
 
 if __name__ == "__main__":
-    test_animal = Animal.Lizard
+    test_animal = random.choice(list(Animal))
     print(test_animal.name)
     print(DB)
