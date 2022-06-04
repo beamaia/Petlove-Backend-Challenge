a6a7ad2c496ba05c6b5c3094711c2a63d3d2f8d0
Bea Maia <beamaia.work@gmail.com>
2022-06-04 11:23:17 -0300

    :sparkles: create services type
    Co-Authored-By: Sophie Dilhon <51868657+ahalic@users.noreply.github.com>

---
+++ database/services.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Services(Enum):
+    bath = "Bath"
+    grooming = "Grooming"
+    appointment = "Appointment"
+    vaccination = "Vaccination"
+    blood_test = "Blood Test"
+    ultrassound_scan = "Ultrassound Scan"
+    xray_exam = "X-ray Exam"
