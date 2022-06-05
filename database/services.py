bddcd8b8d4e540b7fca77e2c0874cbc6200bd6e3
AHalic <sophie.gama@aluno.ufes.br>
2022-06-05 12:14:47 -0300

    :hammer: updates services and prices list

--- database/services.py
+++ database/services.py
@@ -1,10 +1,8 @@
-from enum import Enum
-
-class Services(Enum):
-    bath = "Bath"
-    grooming = "Grooming"
-    appointment = "Appointment"
-    vaccination = "Vaccination"
-    blood_test = "Blood Test"
-    ultrassound_scan = "Ultrassound Scan"
-    xray_exam = "X-ray Exam"
+Services = [{'service': 'Bath', 'price': 50}, 
+            {'service': 'Grooming', 'price': 65},
+            {'service': 'Appointment', 'price': 100},
+            {'service': 'Vaccination', 'price': 150},
+            {'service': 'Blood Test', 'price': 125},
+            {'service': 'Ultrassound Scan', 'price': 245},
+            {'service': 'X-ray Exam', 'price': 180},
+            {'service': 'Cardiac Exam', 'price': 170}]
