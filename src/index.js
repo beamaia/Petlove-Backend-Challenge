b8b6c02241b5e2b7db638c3adbdb0221a65a9c9b
Bea Maia <beamaia.work@gmail.com>
2022-06-09 21:32:05 -0300

    :construction: makes app listen
    Co-Authored-By: Sophie Dilhon <51868657+ahalic@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-const db = require('./db');
-const { query } = require('express');
+const customExpress = require('./config/customExpress')
+const connection = require('./database/db')
+const express = require('express')
 
-console.log(db.query('SHOW DATABASE'))
+/**
+ * Conecta com o servidor backend e cria as tabelas do bd
+ */
+// connection.connect(erro => {
+//     if(erro) {
+//         console.log(erro);
+//     } else {
+//         console.log('Connected successfully');
+        
+
+//         app.use('/', express.static(__dirname + '/api/'));
+//         app.listen(8080, () => console.log('Servidor Backend, rodando na porta 8080'));
+//     }
+// })
+
+const app = customExpress();
+app.listen(8080, () => {
+    console.log('Servidor online na porta', 8080);
+})
