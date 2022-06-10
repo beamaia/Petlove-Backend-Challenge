c6e036672718f36ee442dc1b1a603f3a082f6c7a
AHalic <sophie.gama@aluno.ufes.br>
2022-06-09 23:29:57 -0300

    :ambulance: fixes server listener error

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-const customExpress = require('./config/customExpress')
-const connection = require('./database/db')
-const express = require('express')
+const app = require('./config/customExpress')
 
-/**
- * Conecta com o servidor backend e cria as tabelas do bd
- */
-// connection.connect(erro => {
-//     if(erro) {
-//         console.log(erro);
-//     } else {
-//         console.log('Connected successfully');
-        
 
-//         app.use('/', express.static(__dirname + '/api/'));
-//         app.listen(8080, () => console.log('Servidor Backend, rodando na porta 8080'));
-//     }
-// })
-
-const app = customExpress();
 app.listen(8080, () => {
     console.log('Servidor online na porta', 8080);
 })
