254e7d8fbded16feebc1fd8affe5b7cecc90c5cd
AHalic <sophie.gama@aluno.ufes.br>
2022-06-11 15:20:39 -0300

    :construction: :sparkles: adds route to get person by id

--- src/controller/person.js
+++ src/controller/person.js
@@ -6,4 +6,7 @@ const Person = require('../model/Person')
 // Returns all persons from database
 router.get('/person', Person.getAll)
 
+// Returns a specific person from database
+router.get('/person/:id', Person.get)
+
 module.exports = router
