8d5469cfa2863d6cd76b6f2e3aa7ab5dc08e696a
AHalic <sophie.gama@aluno.ufes.br>
2022-06-11 16:37:24 -0300

    :construction: :sparkles: adds get person's pet route

--- src/controller/person.js
+++ src/controller/person.js
@@ -9,4 +9,7 @@ router.get('/person', Person.getAll)
 // Returns a specific person from database
 router.get('/person/:id', Person.get)
 
+// Returns a specific person's pets from database
+router.get('/person/:id/animal', Person.getPets.bind(Person))
+
 module.exports = router
