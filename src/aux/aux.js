f96a3ffb5b83a4592308caf0175d2749673437b6
AHalic <sophie.gama@aluno.ufes.br>
2022-06-11 18:44:21 -0300

    :sparkles: adds auxiliary functions

---
+++ src/aux/aux.js
@@ -0,0 +1,19 @@
+function getCountOfDigits(str) {
+    return str.replace(/[^0-9]/g, '').length;
+}
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+module.exports = {
+    getCountOfDigits,
+    getAge
+}
