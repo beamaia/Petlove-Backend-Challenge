c899e5b7ea4ae0dfa4fc3e67f08d1cf77a1b01db
AHalic <sophie.gama@aluno.ufes.br>
2022-06-11 15:20:17 -0300

    :construction: :sparkles: adds query to get person by id

--- src/model/person.js
+++ src/model/person.js
@@ -10,11 +10,36 @@ class Person {
     getAll(req, res) {
         const sql = `SELECT * FROM Person;`;
 
-        db.query(sql, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
+        db.query(sql, (error, results) => {
+            if(error) {
+                res.status(400).json(error)
             } else {
-                res.status(200).json(resultados.rows)
+                res.status(200).json(results.rows)
+            }
+        })
+    }
+
+    /**
+     * Returns a specific person from database
+     * @param {*} req request containing person's id
+     * @param {*} res 
+     */
+    get(req, res) {
+        let id = req.params.id
+
+        if (isNaN(id)) {
+            return res.status(400).json("Invalid Id");
+        }
+
+        const sql = `SELECT * FROM Person WHERE cpf='${id}'`
+
+        db.query(sql, (error, results) => {
+            if(error) {
+                res.status(400).json(error);
+            } else if (!results.rowCount) {
+                res.status(204).json(`There is no person with cpf as ${id}`);
+            } else {
+                res.status(200).json(results.rows);
             }
         })
     }
