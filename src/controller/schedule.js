27efa5807543ac08c843d31ad61138111799b765
AHalic <sophie.gama@aluno.ufes.br>
2022-06-12 10:56:56 -0300

    :sparkles: :construction: adds get routes

--- src/controller/schedule.js
+++ src/controller/schedule.js
@@ -4,6 +4,16 @@ const router = express.Router()
 const Schedule = require('../model/schedule')
 
 // Adds a new schedule
-router.post('/schedule', Schedule.create.bind(Schedule))
+router.post('/schedule', Schedule.create)
+
+// Returns all schedules before today from database
+router.get('/scheduleHistory', function (req, res) {
+    Schedule.get(req, res, 'history')
+})
+
+// Returns all future schedules from database
+router.get('/schedule', function (req, res) {
+    Schedule.get(req, res, 'today')
+})
 
 module.exports = router
