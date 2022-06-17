6145d078e08fadecf28bf32b98bd8f633b402bf3
Bea Maia <beamaia.work@gmail.com>
2022-06-16 22:50:13 -0300

    :sparkles: adds get by id

--- src/model/service.js
+++ src/model/service.js
@@ -1,6 +1,30 @@
 const db = require('../database/db');
 
 class Service {
+
+    /**
+     * Returns services from database by id
+     * @param {*} req
+     * @param {*} res 
+     */
+    get(req, res) {
+        let id = req.params.id
+
+        if (isNaN(id)) {
+            return res.status(400).json("Invalid Id");
+        }
+
+        const sql = `SELECT * FROM service WHERE id_service='${id}';`;
+
+        db.query(sql, (error, results) => {
+            if(error) {
+                res.status(400).json(error)
+            } else {
+                res.status(200).json(results.rows)
+            }
+        })
+    }
+
     /**
      * Returns all services from database
      * @param {*} req
