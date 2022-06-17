ad4f9e7da3503dcfd166bd137680ab0981c19f00
Bea Maia <beamaia.work@gmail.com>
2022-06-16 23:11:15 -0300

    :sparkles: adds delete method

--- src/model/service.js
+++ src/model/service.js
@@ -104,6 +104,30 @@ class Service {
         })
     }
     
+    /**
+         * Deletes a service from database
+         * @param {*} req request containing service's id
+         * @param {*} res
+         **/ 
+    delete(req, res) {
+        let id = req.params.id
+
+        if (isNaN(id)) {
+            return res.status(400).json("Invalid Id");
+        }
+
+        const sql = `DELETE FROM Service WHERE id_service='${id}' RETURNING *`
+
+        db.query(sql, (error, results) => {
+            if(error) {
+                res.status(400).json(error)
+            } else if (!results.rowCount) {
+                res.status(204).json(`There is no service with id_service as ${id}`)
+            } else {
+                res.status(200).json(results)
+            }
+        })
+    }
 
     /**
      * Returns all services from database
