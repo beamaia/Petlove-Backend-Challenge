60c33d26df2a9099635e3588e5c2a75874965d45
AHalic <sophie.gama@aluno.ufes.br>
2022-06-22 19:36:18 -0300

    :sparkles: :test_tube: adds post route tests obs: >200 test doesnt end

--- tests/animalType.test.js
+++ tests/animalType.test.js
@@ -56,6 +56,7 @@ describe('GET /animalType/:id', () => {
 
 });    
 
+// Tests post route for animalType
 describe('POST /animalType', () => {
     test('posts a new animal type', async () => {
         const response = await request(app)
@@ -92,8 +93,61 @@ describe('POST /animalType', () => {
             expect(response.header['content-type']).toBe('application/json; charset=utf-8');
     })
 
-    // teste animal type repetido
-    // teste id repetido
-    // teste id não numérico
-    // teste animal type tamanho máximo
+    test('posts an animal type already inserted', async () => {
+        const response = await request(app)
+            .post('/animalType')
+            .send({
+                type: 'Lion'
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+    })
+
+    test('returns error if id is already in use', async () => {
+        const response = await request(app)
+            .post('/animalType')
+            .send({
+                id_type: 1,
+                type: 'Tiger'
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+            expect(response.body.detail).toEqual('Key (id_type)=(1) already exists.');
+    })
+
+    test('returns error if id is not a number', async () => {
+        const response = await request(app)
+            .post('/animalType')
+            .send({
+                id_type: 'a',
+                type: 'Tiger'
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+    })
+
+    test('returns error if animal type has more than 200 caracters', async () => {
+        const response = await request(app)
+            .post('/animalType')
+            .send({
+                type: 'L'+'ion'.repeat(67)
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+    })
+
+    test('returns error if animal type doesnt follow regex constraint', async () => {
+        const response = await request(app)
+            .post('/animalType')
+            .send({
+                type: 'lion'
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+    })
 })
