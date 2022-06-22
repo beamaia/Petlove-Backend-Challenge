208770e0873a779ad908c2d426b1319d9ea9cdcf
AHalic <sophie.gama@aluno.ufes.br>
2022-06-22 19:03:28 -0300

    :recycle: fixes names based on constraint

--- database/services.py
+++ database/services.py
@@ -3,7 +3,7 @@ Services = [{'service': 'Bath', 'price': 50},
             {'service': 'Grooming', 'price': 65},
             {'service': 'Appointment', 'price': 100},
             {'service': 'Vaccination', 'price': 150},
-            {'service': 'Blood Test', 'price': 125},
-            {'service': 'Ultrassound Scan', 'price': 245},
-            {'service': 'X-ray Exam', 'price': 180},
-            {'service': 'Cardiac Exam', 'price': 170}]
+            {'service': 'Blood test', 'price': 125},
+            {'service': 'Ultrassound scan', 'price': 245},
+            {'service': 'Xray exam', 'price': 180},
+            {'service': 'Cardiac exam', 'price': 170}]
