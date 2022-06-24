ccc04a63a1eb0e92391d30b943db8c529b49f872
Bea Maia <beamaia.work@gmail.com>
2022-06-23 21:05:16 -0300

    :sparkles: :recycle:  :construction: adds schedules tests

--- tests/person.test.js
+++ tests/person.test.js
@@ -16,7 +16,7 @@ describe('GET /person', () => {
         expect(response.body).toHaveLength(100);
         expect(response.header['content-type']).toBe('application/json; charset=utf-8');
     })
-});    
+})
 
 // Tests get by id route for person
 describe('GET /person/:id', () => {
@@ -72,7 +72,7 @@ describe('GET /person/:id/animal', () => {
             .get('/person/91165531253/animal');
 
         expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(3);
+        expect(response.body).toHaveLength(1);
 
         // Checks if each element has property pet #1
         expect(response.body[0]).toHaveProperty('id_animal');
@@ -81,53 +81,125 @@ describe('GET /person/:id/animal', () => {
         expect(response.body[0]).toHaveProperty('name');
         expect(response.body[0]).toHaveProperty('date_birth');
 
-        // Checks if each element has property pet #2
-        expect(response.body[0]).toHaveProperty('id_animal');
-        expect(response.body[0]).toHaveProperty('id_person');
-        expect(response.body[0]).toHaveProperty('id_type');
-        expect(response.body[0]).toHaveProperty('name');
-        expect(response.body[0]).toHaveProperty('date_birth');
-
-        // Checks if each element has property pet #3
-        expect(response.body[0]).toHaveProperty('id_animal');
-        expect(response.body[0]).toHaveProperty('id_person');
-        expect(response.body[0]).toHaveProperty('id_type');
-        expect(response.body[0]).toHaveProperty('name');
-        expect(response.body[0]).toHaveProperty('date_birth');
-
         // Checks values
-        expect(response.body[0].id_animal).toBe(3);
+        expect(response.body[0].id_animal).toBe(78);
         expect(response.body[0].id_person).toBe('91165531253');
-        expect(response.body[0].id_type).toBe(31);
-        expect(response.body[0].name).toBe('Elisa');
-        expect(response.body[0].date_birth).toContain('2019-01-17');
-
-        expect(response.body[1].id_animal).toBe(41);
-        expect(response.body[1].id_person).toBe('91165531253');
-        expect(response.body[1].id_type).toBe(14);
-        expect(response.body[1].name).toBe('Calebe');
-        expect(response.body[1].date_birth).toContain('2015-07-19');
-
-        expect(response.body[2].id_animal).toBe(96);
-        expect(response.body[2].id_person).toBe('91165531253');
-        expect(response.body[2].id_type).toBe(19);
-        expect(response.body[2].name).toBe('Letícia');
-        expect(response.body[2].date_birth).toContain('2012-10-13');
+        expect(response.body[0].id_type).toBe(4);
+        expect(response.body[0].name).toBe('Enzo');
+        expect(response.body[0].date_birth).toContain('2019-03-10');
 
         expect(response.header['content-type']).toBe('application/json; charset=utf-8');
         
     })
 
-    
+    // TODO: post person beforehand
+    // test('returns empty if person does not exist', async () => {
+    //     const response = await request(app)
+    //         .get('/person/11111111111');
+
+    //         expect(response.status).toBe(204);
+    //         expect(response.body).toEqual({});
+    //         expect(response.body).toEqual("The person with cpf as ${id} has no pet")
+    // })
+
+
+    test('returns error if cpf is not a number', async () => {
+        const response = await request(app)
+            .get('/person/a/animal');
+            expect(response.status).toBe(400);
+            expect(response.body).toEqual('Invalid Id');
+    })
+
+    test('returns message if cpf doesnt exist', async () => {
+        const response = await request(app)
+            .get('/person/22222222222/animal');
+
+            expect(response.status).toBe(204);
+            expect(response.body).toEqual({});
+    })
+
+})
+
+
+// Tests to get a specific person's pets schedule from database
+describe('GET /person/:id/scheduleHistory', () => {
+    test('returns persons 76612908200 scheduled history', async () => {
+        const response = await request(app)
+            .get('/person/76612908200/scheduleHistory');
+        
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(2);
+
+        // Checks if each element has property schedule #1
+        expect(response.body[0]).toHaveProperty('id_schedule');
+        expect(response.body[0]).toHaveProperty('id_animal');
+        expect(response.body[0]).toHaveProperty('id_person');
+        expect(response.body[0]).toHaveProperty('id_service');
+        expect(response.body[0]).toHaveProperty('date_service');
+
+        expect(response.body[0].id_schedule).toBe(130);
+        expect(response.body[0].id_animal).toBe(146);
+        expect(response.body[0].id_person).toBe('76612908200');
+        expect(response.body[0].id_service).toBe(8);
+        expect(response.body[0].date_service).toBe('2021-09-01T15:30:00.000Z');
+
+
+        expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+    })
+
+    test('returns error if cpf is not a number', async () => {
+        const response = await request(app)
+            .get('/person/a/scheduleHistory');
+            expect(response.status).toBe(400);
+            expect(response.body).toEqual('Invalid Id');
+    })
+
+    test('returns message if cpf doesnt exist', async () => {
+        const response = await request(app)
+            .get('/person/22222222222/scheduleHistory');
+
+            expect(response.status).toBe(204);
+            expect(response.body).toEqual({});
+    })
+
+    // TODO must post animal before
+    // test('returns message if cpf doesnt have anything scheduled', async () => {
+    //     const response = await request(app)
+    //         .get('/person/22222222222/scheduleHistory');
+
+    //         expect(response.status).toBe(204);
+    //         expect(response.body).toEqual({});
+    // })
+})
+
+describe('GET /person/:id/schedule', () => {
+    test('returns person 81463743911 current schedule', async () => {
+        const response = await request(app)
+            .get('/person/81463743911/schedule');
+        
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(3);
+
+        // Checks if each element has property schedule #1
+        expect(response.body[0]).toHaveProperty('id_schedule');
+        expect(response.body[0]).toHaveProperty('id_animal');
+        expect(response.body[0]).toHaveProperty('id_person');
+        expect(response.body[0]).toHaveProperty('id_service');
+        expect(response.body[0]).toHaveProperty('date_service');
+
+        expect(response.body[0].id_schedule).toBe(183);
+        expect(response.body[0].id_animal).toBe(11);
+        expect(response.body[0].id_person).toBe('81463743911');
+        expect(response.body[0].id_service).toBe(4);
+        expect(response.body[0].date_service).toBe('2023-03-03T18:00:00.000Z');
+
+
+        expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+    })
 })
 
 // // Returns a specific person's pets from database
 // router.get('/person/:id/animal', Person.getPets)
-
-// // Returns a specific person's pets schedule from database
-// router.get('/person/:id/schedule', function (req, res) {
-//     Person.getSchedule(req, res, 'future')
-// })
 
 // // Returns a specific person's pets schedule history from database
 // router.get('/person/:id/scheduleHistory', function (req, res) {
